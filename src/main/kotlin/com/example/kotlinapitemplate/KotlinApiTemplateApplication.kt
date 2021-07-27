8d23df65f85be4e3d34a0c85aa333047594c372c
ByeongUk Choi <cbw0916@gmail.com>
2021-07-27 19:55:22 +0900

    init spring source

---
+++ src/main/kotlin/com/example/kotlinapitemplate/KotlinApiTemplateApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinapitemplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinApiTemplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinApiTemplateApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinapitemplate/KotlinApiTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinapitemplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinApiTemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
