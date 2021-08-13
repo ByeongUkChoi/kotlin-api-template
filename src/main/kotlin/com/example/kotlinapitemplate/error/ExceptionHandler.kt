9516a1b0b62bf66df63fca33c9e3b3fc205a714e
ByeongUk Choi <cbw0916@gmail.com>
2021-08-14 01:47:30 +0900

    Add missing request values exception handler

--- src/main/kotlin/com/example/kotlinapitemplate/error/ExceptionHandler.kt
+++ src/main/kotlin/com/example/kotlinapitemplate/error/ExceptionHandler.kt
@@ -1,15 +1,26 @@
 package com.example.kotlinapitemplate.error
 
 import com.example.kotlinapitemplate.error.exception.BusinessException
+import com.example.kotlinapitemplate.error.exception.ErrorCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import javax.validation.UnexpectedTypeException
 
 @ControllerAdvice
 class ExceptionHandler {
 
+    /**
+     * 컨트롤러 필수값 누락 예외 처리
+     */
+    @ExceptionHandler(UnexpectedTypeException::class)
+    fun requestParamInvalidExceptionHandle(e: UnexpectedTypeException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity(ErrorResponse(ErrorCode.MISSING_REQUIRED_VALUES.code, ErrorCode.MISSING_REQUIRED_VALUES.message), ErrorCode.MISSING_REQUIRED_VALUES.httpStatus)
+    }
+
     @ExceptionHandler(BusinessException::class)
-    fun handle(e: BusinessException): ResponseEntity<ErrorResponse> {
+    fun businessExceptionHandle(e: BusinessException): ResponseEntity<ErrorResponse> {
         return ResponseEntity(ErrorResponse(e.getCode(), e.getErrorMessage()), e.getHttpStatus())
     }
+
 }
--- src/main/kotlin/com/example/kotlinapitemplate/error/exception/ErrorCode.kt
+++ src/main/kotlin/com/example/kotlinapitemplate/error/exception/ErrorCode.kt
@@ -3,5 +3,6 @@ package com.example.kotlinapitemplate.error.exception
 import org.springframework.http.HttpStatus
 
 enum class ErrorCode(val code: Int, val httpStatus: HttpStatus, val message: String) {
-    NOT_FOUND_ORDER(1001, HttpStatus.NOT_FOUND, "주문 정보를 찾을 수 없습니다.")
+    MISSING_REQUIRED_VALUES(1001, HttpStatus.BAD_REQUEST, "필수 값이 누락되었습니다."),
+    NOT_FOUND_ORDER(2001, HttpStatus.NOT_FOUND, "주문 정보를 찾을 수 없습니다.")
 }
