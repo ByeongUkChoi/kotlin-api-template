1eb09ff3108e426ecd9e82b9f7612d6cfa72a550
ByeongUk Choi <cbw0916@gmail.com>
2021-08-19 00:14:43 +0900

    Add get order api failure test

--- src/test/kotlin/com/example/kotlinapitemplate/order/api/GetOrderTest.kt
+++ src/test/kotlin/com/example/kotlinapitemplate/order/api/GetOrderTest.kt
@@ -7,35 +7,19 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class GetOrdersTest {
+class GetOrderTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
     @Test
-    fun getOrdersTest() {
-        // given
-        val userId = "cbw"
-
+    fun `when get order api not found order failure test`() {
         // when & then
         mockMvc.perform(
-            get("/orders")
-                .accept(MediaType.APPLICATION_JSON)
-                .header("X-USER-ID", userId)
-        )
-            .andExpect(status().isOk)
-            .andExpect(content().json("[]"))
-    }
-
-    @Test
-    fun `when get orders api without header failure test`() {
-        // when & then
-        mockMvc.perform(
-            get("/orders")
+            get("/orders/9999")
                 .accept(MediaType.APPLICATION_JSON)
         )
             .andExpect(status().isBadRequest)
