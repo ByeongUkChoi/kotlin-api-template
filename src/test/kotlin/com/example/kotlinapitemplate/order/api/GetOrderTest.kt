4573ec351d4eefcdd6fa099a12849d65ae98269e
ByeongUk Choi <cbw0916@gmail.com>
2021-08-20 00:30:53 +0900

    fix get order api failure test

--- src/test/kotlin/com/example/kotlinapitemplate/order/api/GetOrderTest.kt
+++ src/test/kotlin/com/example/kotlinapitemplate/order/api/GetOrderTest.kt
@@ -1,5 +1,7 @@
 package com.example.kotlinapitemplate.order.api
 
+import com.example.kotlinapitemplate.error.exception.ErrorCode
+import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -7,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
@@ -16,12 +19,28 @@ class GetOrderTest {
     private lateinit var mockMvc: MockMvc
 
     @Test
+    fun `when get order api without header failure test`() {
+        // TODO: add exception
+
+        // when & then
+        mockMvc.perform(
+            get("/orders/1")
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isBadRequest)
+    }
+
+    @Test
     fun `when get order api not found order failure test`() {
         // when & then
         mockMvc.perform(
             get("/orders/9999")
                 .accept(MediaType.APPLICATION_JSON)
+                .header("X-USER-ID", "cbw")
         )
-            .andExpect(status().isBadRequest)
+            .andExpect(status().isNotFound)
+            .andExpect(jsonPath("$.code", `is`(ErrorCode.NOT_FOUND_ORDER.code)))
+            .andExpect(jsonPath("$.message", `is`(ErrorCode.NOT_FOUND_ORDER.message)))
     }
+
 }
