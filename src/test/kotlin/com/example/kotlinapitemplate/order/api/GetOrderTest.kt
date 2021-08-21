c1808dbc1ddf3ed7249dc795b7ed409a30ebfd1e
ByeongUk Choi <cbw0916@gmail.com>
2021-08-21 21:24:05 +0900

    add get order api test with sql script

--- src/test/kotlin/com/example/kotlinapitemplate/order/api/GetOrderTest.kt
+++ src/test/kotlin/com/example/kotlinapitemplate/order/api/GetOrderTest.kt
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
@@ -17,6 +18,29 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 class GetOrderTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
+
+    @Test
+    @Sql(statements = ["INSERT INTO ORDERS(ID, ORDERER_ID, PRODUCT_ID, QUANTITY, TOTAL_PRICE) VALUES (1001, 'get-order-test-1', 2, 3, 4000)"])
+    fun getOrderTest() {
+        // given (sql script)
+        val userId = "get-order-test-1"
+        val orderId = 1001L
+        val productId = 2L
+        val quantity = 3
+        val totalPrice = 4000L
+
+        // when & then
+        mockMvc.perform(
+            get("/orders/$orderId")
+                .accept(MediaType.APPLICATION_JSON)
+                .header("X-USER-ID", userId)
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.id", `is`(orderId.toInt())))
+            .andExpect(jsonPath("$.productId", `is`(productId.toInt())))
+            .andExpect(jsonPath("$.quantity", `is`(quantity)))
+            .andExpect(jsonPath("$.totalPrice", `is`(totalPrice.toInt())))
+    }
 
     @Test
     fun `when get order api without header failure test`() {
