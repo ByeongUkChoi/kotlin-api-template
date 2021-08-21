f4ee8c8b9b2bf88745392b5a398c2dde46d43f72
ByeongUk Choi <cbw0916@gmail.com>
2021-08-21 23:49:03 +0900

    add error code

--- src/main/kotlin/com/example/kotlinapitemplate/error/exception/ErrorCode.kt
+++ src/main/kotlin/com/example/kotlinapitemplate/error/exception/ErrorCode.kt
@@ -4,5 +4,6 @@ import org.springframework.http.HttpStatus
 
 enum class ErrorCode(val code: Int, val httpStatus: HttpStatus, val message: String) {
     MISSING_REQUIRED_VALUES(1001, HttpStatus.BAD_REQUEST, "필수 값이 누락되었습니다."),
+    MISSING_REQUEST_HEADER(1002, HttpStatus.FORBIDDEN, "헤더 값이 누락되었습니다."),
     NOT_FOUND_ORDER(2001, HttpStatus.NOT_FOUND, "주문 정보를 찾을 수 없습니다.")
 }
